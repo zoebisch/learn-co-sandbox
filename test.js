2cbc4cdd9f956a3e1a44b4f7a0c02a3b83fad16f
Brian Zoebisch <zoebisch@gmail.com>
2019-01-13 18:50:12 +0000

    Automatically backed up by Learn

---
+++ test.js
@@ -0,0 +1,9 @@
+let state={count:0}
+function reducer(state, action){
+  switch (action.type) {
+    case 'INCREASE_COUNT': 
+      return{count:state.count +1};
+    default:
+      return state;
+  }
+}
